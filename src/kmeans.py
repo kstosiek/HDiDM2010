99fba86b9dc0eaf86fcc5b07ad4cbdff51977726
Karol Stosiek <karol.stosiek@gmail.com>
2010-04-30 19:05:54 +0200

    Added option for treating data as differential data

--- src/kmeans.py
+++ src/kmeans.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 	# Parse command line arguments.
 
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hc:o:i:d:", 
+		opts, args = getopt.getopt(sys.argv[1:], "hc:o:i:d:D", 
 				["help", "output=", "clusters-num=", "iters-num=", 
 				 "dist="])
 	except getopt.GetoptError, err:
@@ -21,6 +21,7 @@ if __name__ == "__main__":
 	number_of_clusters = 10
 	number_of_iters = 100
 	dist_measure = 'e'
+        treat_data_differentially = False
 
 	for option, arg in opts:
 		if option in ("-h", "--help"):
@@ -30,7 +31,8 @@ if __name__ == "__main__":
 			print "  -c, --clusters-num <num>   (default: 10)"
 			print "  -i, --iters-num <num>      (default: 100)"
 			print "  -d, --distance <char>      (default: e)"
-			sys.exit(1)
+			print "  -D, --differential         (default: true)"
+                        sys.exit(1)
 		elif option in ("-o", "--output"):
 			output_file_path = arg
 		elif option in ("-c", "--clusters-num"):
@@ -39,19 +41,27 @@ if __name__ == "__main__":
 			number_of_iters = int(arg)
 		elif option in ("-d", "--distance"):
 			dist_measure = arg
+                elif option in ("-D", "--differentially"):
+                        treat_data_differentially = True;
+                        
 			
-	print "Number of clusters is", number_of_clusters
-	print "Output file is", output_file_path
-	print "Number of iterations is", number_of_iters
-	print "Distance measure is", dist_measure
+	print "Number of clusters is ", number_of_clusters
+	print "Output file is ", output_file_path
+	print "Number of iterations is ", number_of_iters
+	print "Distance measure is ", dist_measure
+        print "Data treated as differential: ", treat_data_differentially
 
 	# Prepare data. I'm not giving possibilty of defining
 	# different input data file via command line, I think 
 	# there's no need for it now.
 
 	data = utils.parse_data("../data/notowania.txt")
-
-	price_vecs = utils.make_prices_vecs(data)
+      
+        price_vecs = []
+        if (treat_data_differentially):
+		price_vecs = utils.make_prices_diffs_vecs(data)
+        else:
+		price_vecs = utils.make_prices_vecs(data)
 
 	# FIXME: Place for vector preprocessing is somewhere here eg.
 	# computing prices differences vector.
