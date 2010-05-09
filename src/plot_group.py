988511e65d5d262594d523bec16ae43bea627e72
dreamer_ <dreamer.tan@gmail.com>
2010-05-09 07:02:08 +0200

    printing all cluster groups in different colours

--- src/plot_group.py
+++ src/plot_group.py
@@ -3,35 +3,30 @@ import utils
 import sys
 import os
 
-if __name__ == "__main__":
+def generate_gnuplot_command(files_list):
+	if not files_list:
+		return ''
+	colours_str = """
+	blue red gold green navyblue violet salmon
+	#87CEEB #8B008B #FFA07A #00FF00 #800000 #808000 #4169E1 #708090 
+	"""
+	colours = colours_str.split()
+	plots_colours = zip(files_list, colours)
+	plots = [ '"%s" lt rgb "%s" with lines'%pair for pair in plots_colours]
+	return 'plot %s\n' % ', '.join(plots)
 
-	if len(sys.argv) != 3:
-		print "Usage: python plot_group.py <clusters file> <cluster number>"
-		sys.exit(1)
-	
+def detect_cluster_number(clusters_file_path):
+	n = 0
+	for line in open(clusters_file_path):
+		line = line.strip()
+		if(line.isdigit()):
+			tmp_n = int(line)
+			if tmp_n > n:
+				n = tmp_n
+	return n+1
 
-	# Parse command line arguments.
-
-	err_msg = ""
-
-	try:
-		clusters_file_path = sys.argv[1]
-		clusters_file = open(clusters_file_path, "r")	
-		cluster_number = int(sys.argv[2])
-	except ValueError:	
-		err_msg = "error: wrong cluster number"
-	except IOError:
-		err_msg = "error: couldn't open file with clusters"
-	
-	# If error message is not empty print it and exit.
-	
-	if err_msg != "":
-		print err_msg
-		sys.exit(1)
-
-	# Parse stock data.
-
-	data = utils.parse_data("../data/notowania.txt")
+def plot_single_cluster(clusters_file_path, cluster_number):
+	clusters_file = open(clusters_file_path, "r")	
 
 	# Create temporary file for plot data.
 
@@ -73,28 +68,92 @@ if __name__ == "__main__":
 	# file before running gnuplot.
 
 	plot_data_tmpfile.close()
+	clusters_file.close()
+	return found_cluster, plot_data_tmpfile_path
+
+
+if __name__ == "__main__":
+
+	if len(sys.argv) != 3:
+		print "Usage: python plot_group.py <clusters file> <cluster number>"
+		print "Usage: python plot_group.py <clusters file> all"
+		sys.exit(1)
+	
+
+	# Parse command line arguments.
+
+	err_msg = ""
+	all_clusters = False
+
+	try:
+		clusters_file_path = sys.argv[1]
+		clusters_file = open(clusters_file_path, "r")	
+		clusters_file.close()
+		if(sys.argv[2] == "all"):
+			all_clusters = True
+		else:
+			cluster_number = int(sys.argv[2])
+	except ValueError:	
+		err_msg = "error: wrong cluster number"
+	except IOError:
+		err_msg = "error: couldn't open file with clusters"
+	
+	# If error message is not empty print it and exit.
+	
+	if err_msg != "":
+		print err_msg
+		sys.exit(1)
+
+	# Parse stock data.
+
+	data = utils.parse_data("../data/notowania.txt")
+
+	found_cluster = False
+	plot_tmpfiles = []
+	if all_clusters:
+		cluster_number = detect_cluster_number(clusters_file_path)
+		for i in range(cluster_number):
+			_, plot_data = plot_single_cluster(clusters_file_path, i)
+			plot_tmpfiles.append(plot_data)
+	else:
+		found_cluster, plot_data_tmpfile_path = plot_single_cluster(clusters_file_path, cluster_number)
 	
 	if found_cluster:
 
 		# Generate file with commands for gnuplot.
 
-		gnuplot_commands_tmpfile_path = tempfile.mkstemp()[1]	
+		gnuplot_commands_tmpfile_path = tempfile.mkstemp()[1]
 		gnuplot_commands_tmpfile = open(gnuplot_commands_tmpfile_path, "r+")
 		gnuplot_commands_tmpfile.write("plot '" + plot_data_tmpfile_path +
-				"' with lines\n")
+				"'  with lines \n")
 		gnuplot_commands_tmpfile.write("pause mouse any")
 		gnuplot_commands_tmpfile.close()
 
 		print "Click in the plot to exit... (hit Ctrl-C if hangs)"
 
+		print gnuplot_commands_tmpfile_path
 		os.system("gnuplot " + gnuplot_commands_tmpfile_path)
 
 		gnuplot_commands_tmpfile.close()
 		os.remove(gnuplot_commands_tmpfile_path)
 	
-	else:
+		os.remove(plot_data_tmpfile_path)
+	elif all_clusters:
+		plot_cmd = generate_gnuplot_command(plot_tmpfiles)
+		#print plot_cmd
 
+		gnuplot_commands_tmpfile_path = tempfile.mkstemp()[1]
+		gnuplot_commands_tmpfile = open(gnuplot_commands_tmpfile_path, "r+")
+		gnuplot_commands_tmpfile.write(plot_cmd)
+		gnuplot_commands_tmpfile.write("pause mouse any")
+		gnuplot_commands_tmpfile.close()
+		print "Click in the plot to exit... (hit Ctrl-C if hangs)"
+		print gnuplot_commands_tmpfile_path
+		os.system("gnuplot " + gnuplot_commands_tmpfile_path)
+		gnuplot_commands_tmpfile.close()
+		os.remove(gnuplot_commands_tmpfile_path)
+		for tmpfile_path in plot_tmpfiles:
+			os.remove(tmpfile_path)
+	else:
 		print "No group with this number."
 	
-	os.remove(plot_data_tmpfile_path)
-	clusters_file.close()
