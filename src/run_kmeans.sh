cf76964da83f4c4c4800fc3d6983a5d02c26072d
Karol Stosiek <karol.stosiek@gmail.com>
2010-06-06 17:53:33 +0200

    Reorganization of generation scripts

--- src/run_kmeans.sh
+++ src/run_kmeans.sh
@@ -1,45 +1,59 @@
 #!/bin/bash
 
-for clusters_num in 5 10 15 20
+for clusters_num in 5 10 15 20  
 do
   mkdir -p ../report/v2/kmeans/$clusters_num
-  rm -rf ../report/v2/kmeans/$clusters_num/*  
+  rm -rf ../report/v2/kmeans/$clusters_num/*
+  
+  xrange="" # dni, np dla wykresu na dni 100..200:  xrange="100:200"
+  events_type="ekonomiczne polityczne katastrofy inne"
+  # katastrofy
+  # polityczne
+  # ekonomiczne
+  # inne
+  output_path="../report/v2/kmeans/"$clusters_num"/"
+  output_file="output_kmeans_"$clusters_num
 
-  output_file="../report/v2/kmeans/"$clusters_num"/output_kmeans_"$clusters_num
   echo "====================================================="
-  echo "RUNNING HIERARCHICAL CLUSTERING FOR ${clusters_num} CLUSTERS"
+  echo "RUNNING KMEANS CLUSTERING FOR ${clusters_num} CLUSTERS"
   echo ""
   python cluster.py ../data/notowania.txt \
-	--output $output_file \
+	--output $output_path$output_file \
     --clusters-num $clusters_num \
     --iters-num 300 \
     --method a \
     --kmeans
-
-  echo "-----------------------------------------------------"
-  echo "PLOTTING SEPARATE CLUSTERS"
-  for ((cluster=0; cluster<clusters_num; cluster++))
-  do
-    plot_output_file=$output_file"_"$cluster
-    python plot_group.py $output_file $plot_output_file $cluster
-  done 
-
-  echo "-----------------------------------------------------"
-  echo "PLOTTING SEPARATE CLUSTERS AVERAGES"
-  for ((cluster=0; cluster<clusters_num; cluster++))
-  do
-    plot_output_file=$output_file"_"$cluster"_average"
-    python plot_group.py $output_file $plot_output_file $cluster average
-  done
   
-  echo "-----------------------------------------------------"
-  echo "PLOTTING ALL INDIVIDUALS"
-  plot_output_file=$output_file"_all"
-  python plot_group.py $output_file $plot_output_file all
+  for event in $events_type
+  do
+    mkdir -p $output_path/$event/
+    rm -rf $output_path/$event/*
+ 
+    plot_output_path=$output_path/$event/
+  	echo "----------------------------------------------------"
+  	echo "PLOTTING SEPARATE CLUSTERS ("$event")"
+  	for ((cluster=0; cluster<clusters_num; cluster++))
+  	do
+    	plot_output_file=$output_file"_"$cluster
+    	python plot_group.py $output_path$output_file $plot_output_path$plot_output_file $cluster events:$event xrange:$xrange
+  	done 
 
-  echo "-----------------------------------------------------"
-  echo "PLOTTING ALL AVERAGES"
-  plot_output_file=$output_file"_all_average"
-  python plot_group.py $output_file $plot_output_file all average
+  	echo "-----------------------------------------------------"
+  	echo "PLOTTING SEPARATE CLUSTERS AVERAGES ("$event")"
+  	for ((cluster=0; cluster<clusters_num; cluster++))
+  	do
+    	plot_output_file=$output_file"_"$cluster"_average"
+    	python plot_group.py $output_path$output_file $plot_output_path$plot_output_file $cluster average events:$event xrange:$xrange
+  	done
+  
+  	echo "-----------------------------------------------------"
+  	echo "PLOTTING ALL INDIVIDUALS ("$event")"
+   	plot_output_file=$output_file"__all"
+  	python plot_group.py $output_path$output_file $plot_output_path$plot_output_file all events:$event xrange:$xrange
 
+  	echo "-----------------------------------------------------"
+  	echo "PLOTTING ALL AVERAGES ("$event")"
+    plot_output_file=$output_file"_all_average"
+  	python plot_group.py $output_path$output_file $plot_output_path$plot_output_file all average events:$event xrange:$xrange
+  done
 done
--- src/run_selforgmaps.sh
+++ src/run_selforgmaps.sh
@@ -1,49 +1,60 @@
 #!/bin/bash
 
-for n in 2 3 4
+for n in 2 3 4  
 do
   clusters_num=$(($n * $n))
   mkdir -p ../report/v2/selforgmaps/$clusters_num
-  rm -rf ../report/v2/selforgmaps/$clusters_num/*  
+  rm -rf ../report/v2/selforgmaps/$clusters_num/*
+  
+  events_type="ekonomiczne polityczne katastrofy inne"
+  # katastrofy
+  # polityczne
+  # ekonomiczne
+  # inne
+  output_path="../report/v2/selforgmaps/"$clusters_num"/"
+  output_file="output_selforgmaps_"$clusters_num
 
-  events_type="ekonomiczne"
-
-  output_file="../report/v2/selforgmaps/"$clusters_num"/output_selforgmaps_"$clusters_num
   echo "====================================================="
-  echo "RUNNING HIERARCHICAL CLUSTERING FOR ${clusters_num} CLUSTERS"
+  echo "RUNNING SELFORGMAPS CLUSTERING FOR ${clusters_num} CLUSTERS"
   echo ""
   python cluster.py ../data/notowania.txt \
-	--output $output_file \
-	--xgrid $n \
-	--ygrid $n \
+	--output $output_path$output_file \
+    --xgrid $n \
+    --ygrid $n \
     --iters-num 5000 \
     --method a \
     --selforgmaps
-
-  echo "-----------------------------------------------------"
-  echo "PLOTTING SEPARATE CLUSTERS"
-  for ((cluster=0; cluster<clusters_num; cluster++))
-  do
-    plot_output_file=$output_file"_"$cluster
-    python plot_group.py $output_file $plot_output_file $cluster events:$events_type
-  done 
-
-  echo "-----------------------------------------------------"
-  echo "PLOTTING SEPARATE CLUSTERS AVERAGES"
-  for ((cluster=0; cluster<clusters_num; cluster++))
-  do
-    plot_output_file=$output_file"_"$cluster"_average"
-    python plot_group.py $output_file $plot_output_file $cluster average events:$events_type
-  done
   
-  echo "-----------------------------------------------------"
-  echo "PLOTTING ALL INDIVIDUALS"
-  plot_output_file=$output_file"_all"
-  python plot_group.py $output_file $plot_output_file all events:$events_type
+  for event in $events_type
+  do
+    mkdir -p $output_path/$event/
+    rm -rf $output_path/$event/*
+ 
+    plot_output_path=$output_path/$event/
+  	echo "----------------------------------------------------"
+  	echo "PLOTTING SEPARATE CLUSTERS ("$event")"
+  	for ((cluster=0; cluster<clusters_num; cluster++))
+  	do
+    	plot_output_file=$output_file"_"$cluster
+    	python plot_group.py $output_path$output_file $plot_output_path$plot_output_file $cluster events:$event xrange:$xrange
+  	done 
 
-  echo "-----------------------------------------------------"
-  echo "PLOTTING ALL AVERAGES"
-  plot_output_file=$output_file"_all_average"
-  python plot_group.py $output_file $plot_output_file all average events:$events_type
+  	echo "-----------------------------------------------------"
+  	echo "PLOTTING SEPARATE CLUSTERS AVERAGES ("$event")"
+  	for ((cluster=0; cluster<clusters_num; cluster++))
+  	do
+    	plot_output_file=$output_file"_"$cluster"_average"
+    	python plot_group.py $output_path$output_file $plot_output_path$plot_output_file $cluster average events:$event xrange:$xrange
+  	done
+  
+  	echo "-----------------------------------------------------"
+  	echo "PLOTTING ALL INDIVIDUALS ("$event")"
+   	plot_output_file=$output_file"__all"
+  	python plot_group.py $output_path$output_file $plot_output_path$plot_output_file all events:$event xrange:$xrange
 
+  	echo "-----------------------------------------------------"
+  	echo "PLOTTING ALL AVERAGES ("$event")"
+    plot_output_file=$output_file"_all_average"
+  	python plot_group.py $output_path$output_file $plot_output_path$plot_output_file all average events:$event xrange:$xrange
+  done
 done
